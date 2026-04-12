anion-examio/foot-mirror

Add support for background blur
This patch adds a new config option: colors{,2}.blur=no|yes. When
enabled, transparent background are also blurred.

Note that this requires the brand new ext-background-effect-v1
protocol, and specifically, that the compositor implements the blur
effect.

terminal.c
@@ -4786,3 +4786,11 @@ term_theme_toggle(struct terminal *term)
     term_damage_margins(term);
     render_refresh(term);
 }
+
+const struct color_theme *
+term_theme_get(const struct terminal *term)
+{
+    return term->colors.active_theme == COLOR_THEME1
+        ? &term->conf->colors
+        : &term->conf->colors2;
+}

tests/test-config.c
@@ -765,6 +765,8 @@ test_section_colors(void)
                    &conf.colors.table[i]);
     }
 
+    test_boolean(&ctx, &parse_section_colors, "blur", &conf.colors.blur);
+
     test_invalid_key(&ctx, &parse_section_colors, "256");
 
     /* TODO: alpha (float in range 0-1, converted to uint16_t) */

mosk-it/foot-vi

terminal.c
@@ -4852,3 +4852,11 @@ term_theme_toggle(struct terminal *term)
     term_damage_margins(term);
     render_refresh(term);
 }
+
+const struct color_theme *
+term_theme_get(const struct terminal *term)
+{
+    return term->colors.active_theme == COLOR_THEME_DARK
+        ? &term->conf->colors_dark
+        : &term->conf->colors_light;
+}

tests/test-config.c
@@ -775,6 +775,8 @@ test_section_colors_dark(void)
                    &conf.colors_dark.table[i]);
     }
 
+    test_boolean(&ctx, &parse_section_colors, "blur", &conf.colors_dark.blur);
+
     test_invalid_key(&ctx, &parse_section_colors_dark, "256");
 
     /* TODO: alpha (float in range 0-1, converted to uint16_t) */
@@ -853,6 +855,8 @@ test_section_colors_light(void)
         test_color(&ctx, &parse_section_colors_light, key_name, false,
                    &conf.colors_light.table[i]);
     }
+
+    test_boolean(&ctx, &parse_section_colors, "blur", &conf.colors_light.blur);
 
     test_invalid_key(&ctx, &parse_section_colors_light, "256");
 
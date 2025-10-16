5cd1527a53b80c8692e5e3dbad3bf9543ce8fbf0
Daniel Eklöf <daniel@ekloef.se>
2025-10-16 13:43:33 +0200

    Add support for background blur
    This patch adds a new config option: colors{,2}.blur=no|yes. When
    enabled, transparent background are also blurred.
    
    Note that this requires the brand new ext-background-effect-v1
    protocol, and specifically, that the compositor implements the blur
    effect.

--- config.c
+++ config.c
@@ -1614,6 +1614,9 @@ parse_color_theme(struct context *ctx, struct color_theme *theme)
             (int *)&theme->dim_blend_towards);
     }
 
+    else if (streq(key, "blur"))
+        return value_to_bool(ctx, &theme->blur);
+
     else {
         LOG_CONTEXTUAL_ERR("not valid option");
         return false;
@@ -3598,6 +3601,7 @@ config_load(struct config *conf, const char *conf_path,
                 .scrollback_indicator = false,
                 .url = false,
             },
+            .blur = false,
         },
         .initial_color_theme = COLOR_THEME_DARK,
         .cursor = {
